e50e6d6fb3ceebba36c88e648d55cb276c989c39
London <lw9153a@student.american.edu>
2021-01-12 22:28:34 -0500

    Ran bivariate analysis and correlations

--- code/01-summary-stat.R
+++ code/01-summary-stat.R
@@ -1,5 +1,6 @@
 library(here)
 library(tidyverse)
+library(PerformanceAnalytics)
 
 climate <- read_csv(here("data", "climate_clean.csv"))
 
@@ -31,4 +32,32 @@ summary(climate$perc_ff)
 
 
 # Bivariate Analysis
+
+ggplot(data = climate) +
+  geom_boxplot(aes(x = as.factor(cp), y = gwvoteimp, fill = as.factor(cp)))+
+  theme_bw()
+
+ggplot(data = climate) +
+  geom_boxplot(aes(x = as.factor(cp), y = perc_ff, fill = as.factor(cp)))+
+  theme_bw()
+
+ggplot(data = climate) +
+  geom_boxplot(aes(x = as.factor(cp), y = perc_renew, fill = as.factor(cp)))+
+  theme_bw()
+
+ggplot(data = climate) +
+  geom_boxplot(aes(x = as.factor(cp), y = human, fill = as.factor(cp)))+
+  theme_bw()
+
+ggplot(data = climate) +
+  geom_point(aes(x = human, y = gwvoteimp))+
+  theme_bw()
+
+climate%>%
+  select(-c(state, state_abb, gov_party, cp_2019, total_energy, renew, ff)) ->
+  climate_num
+
+cor(climate_num)
+
+chart.Correlation(climate_num[,1:10], histogram=TRUE, pch="+")
 
