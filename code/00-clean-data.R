91b6b676f5c3bde11ed8e9c58b1e8966966636ab
London <lw9153a@student.american.edu>
2021-01-12 21:15:12 -0500

    Combinded state and yale data

--- code/00-clean-data.R
+++ code/00-clean-data.R
@@ -1,2 +1,16 @@
 library(here)
+library(tidyverse)
+
+election <- read_csv(here("data", "2016_election.csv"))
+state <- read_csv(here("data", "state_raw.csv"))
+yale <- read_csv(here("data", "yale_data_raw.csv"))
+
+yale%>%
+  filter(GeoType == "State")%>%
+  select(GeoName, gwvoteimp) ->
+  yale
+
+state%>%
+  mutate(cp = ifelse(cp_2019 > 0, 1, 0)) ->
+  state
 
