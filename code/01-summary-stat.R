f460afd45edfb77cc68a84fd77de2ba532645800
London <lw9153a@student.american.edu>
2021-01-12 21:44:48 -0500

    Univariate analysis on first 3 variables

---
+++ code/01-summary-stat.R
@@ -0,0 +1,28 @@
+library(here)
+library(tidyverse)
+
+climate <- read_csv(here("data", "climate_clean.csv"))
+
+# Univariate Analysis
+
+ggplot(climate)+
+  geom_histogram(aes(x = gdp))+
+  theme_bw()
+
+summary(climate$gdp)
+
+ggplot(climate)+
+  geom_histogram(aes(x = co2_emissions))+
+  theme_bw()
+
+summary(climate$co2_emissions)
+
+ggplot(climate)+
+  geom_histogram(aes(x = perc_renew), bins = 15)+
+  theme_bw()
+
+summary(climate$perc_renew)
+
+
+
+# Bivariate Analysis
