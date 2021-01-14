04d47ad4ba5fc924ba54f3e959a27e454b1f90a2
London <lw9153a@student.american.edu>
2021-01-13 19:21:55 -0500

    Added 2nd and 3rd models

--- code/02-linear-models.R
+++ code/02-linear-models.R
@@ -4,17 +4,33 @@ library(broom)
 
 climate <- read_csv(here("data", "clean_climate.csv"))
 
-climate_reg <- lm(gwvoteimp ~ cp + gov_party + co2_emissions + perc_renew + perc_ff, data = climate)
+climate_reg <- lm(gwvoteimp ~ cp + gov_party + co2_emissions
+                  + perc_renew + perc_ff, data = climate)
 summary(climate_reg)
+
+climate_reg2 <- lm(gwvoteimp ~ cp + gov_party + co2_emissions
+                   + perc_renew + perc_ff + pop + perc_white 
+                   + perc_black + perc_latino + perc_asian, 
+                   data = climate)
+summary(climate_reg2)
+
+climate_reg3 <- lm(gwvoteimp ~ human + cp + gov_party 
+                   + co2_emissions + perc_renew + perc_ff 
+                   + pop + perc_white + perc_black + perc_latino 
+                   + perc_asian, data = climate)
+summary(climate_reg3)
 
 
 selcri<-function(lmout)
 {
   n <- length(lmout$fit)
-  rsq <- summary(lmout)$r.sq
   adj.rsq <- summary(lmout)$adj.r.sq
   aic <- extractAIC(lmout)[2]
   bic <- extractAIC(lmout, k = log(n))[2]
   press <- sum((lmout$residuals/(1 - hatvalues(lmout)))^2)
-  cbind(rsq, adj.rsq, aic, bic, press)
+  cbind(adj.rsq, aic, bic, press)
 }
+
+selcri(climate_reg)
+selcri(climate_reg2)
+selcri(climate_reg3)
