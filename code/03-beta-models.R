c9aefa1528f755b4ad9b07c3773482c4df9028a9
London <lw9153a@student.american.edu>
2021-01-13 20:01:08 -0500

    ran first two beta models

--- code/03-beta-models.R
+++ code/03-beta-models.R
@@ -1,4 +1,30 @@
 library(here)
 library(tidyverse)
+library(betareg)
 
 climate <- read_csv(here("data", "clean_climate.csv"))
+
+climate_beta <- lm(gwvoteimp ~ cp + gov_party + co2_emissions
+                   + perc_renew + perc_ff + pop + perc_white 
+                   + perc_black + perc_latino + perc_asian, 
+                   data = climate)
+summary(climate_beta)
+
+climate_beta2 <- lm(gwvoteimp ~ human + cp + gov_party 
+                   + co2_emissions + perc_renew + perc_ff 
+                   + pop + perc_white + perc_black + perc_latino 
+                   + perc_asian, data = climate)
+summary(climate_beta2)
+
+selcri<-function(lmout)
+{
+  n <- length(lmout$fit)
+  adj.rsq <- summary(lmout)$adj.r.sq
+  aic <- extractAIC(lmout)[2]
+  bic <- extractAIC(lmout, k = log(n))[2]
+  press <- sum((lmout$residuals/(1 - hatvalues(lmout)))^2)
+  cbind(adj.rsq, aic, bic, press)
+}
+
+selcri(climate_beta)
+selcri(climate_beta2)
