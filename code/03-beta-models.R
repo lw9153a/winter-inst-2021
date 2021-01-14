2ad690517c673c3e7eb851f9aaf9db9645e60f74
London <lw9153a@student.american.edu>
2021-01-14 15:49:03 -0500

    Matched variables to final model in linear

--- code/03-beta-models.R
+++ code/03-beta-models.R
@@ -4,22 +4,10 @@ library(betareg)
 
 climate <- read_csv(here("data", "clean_climate.csv"))
 
-climate_beta <- betareg(gwvoteimp ~ cp + gov_party + co2_emissions
-                        + perc_renew + perc_ff + perc_black 
+climate_beta <- betareg(gwvoteimp ~ perc_renew + perc_black 
                         + perc_latino + perc_asian + margin_2016, 
                         data = climate)
 summary(climate_beta)
-
-selcri<-function(lmout)
-{
-  n <- length(lmout$fit)
-  adj.rsq <- summary(lmout)$adj.r.sq
-  aic <- extractAIC(lmout)[2]
-  bic <- extractAIC(lmout, k = log(n))[2]
-  cbind(adj.rsq, aic, bic)
-}
-
-selcri(climate_beta)
 
 fitted_beta <- climate_beta$fitted.values
 
