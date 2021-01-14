0bc6d44fbba329afdb397f844a822b556ee2e388
London <lw9153a@student.american.edu>
2021-01-14 15:23:43 -0500

    Adjusted variables in beta model to match final linear model

--- code/03-beta-models.R
+++ code/03-beta-models.R
@@ -5,16 +5,10 @@ library(betareg)
 climate <- read_csv(here("data", "clean_climate.csv"))
 
 climate_beta <- betareg(gwvoteimp ~ cp + gov_party + co2_emissions
-                   + perc_renew + perc_ff + pop + perc_white 
-                   + perc_black + perc_latino + perc_asian, 
-                   data = climate)
+                        + perc_renew + perc_ff + perc_black 
+                        + perc_latino + perc_asian + margin_2016, 
+                        data = climate)
 summary(climate_beta)
-
-climate_beta2 <- betareg(gwvoteimp ~ human + cp + gov_party 
-                   + co2_emissions + perc_renew + perc_ff 
-                   + pop + perc_white + perc_black + perc_latino 
-                   + perc_asian, data = climate)
-summary(climate_beta2)
 
 selcri<-function(lmout)
 {
@@ -22,26 +16,19 @@ selcri<-function(lmout)
   adj.rsq <- summary(lmout)$adj.r.sq
   aic <- extractAIC(lmout)[2]
   bic <- extractAIC(lmout, k = log(n))[2]
-  press <- sum((lmout$residuals/(1 - hatvalues(lmout)))^2)
-  cbind(adj.rsq, aic, bic, press)
+  cbind(adj.rsq, aic, bic)
 }
 
 selcri(climate_beta)
-selcri(climate_beta2)
 
 fitted_beta <- climate_beta$fitted.values
-fitted_beta2 <- climate_beta2$fitted.values
 
 rmse(climate$gwvoteimp, fitted_beta)
-rmse(climate$gwvoteimp, fitted_beta2)
 
 ggplot(data = climate)+
   geom_point(aes(x = fitted_beta, y = gwvoteimp))+
   geom_abline()+
   theme_bw()
 
-ggplot(data = climate)+
-  geom_point(aes(x = fitted_beta2, y = gwvoteimp))+
-  geom_abline()+
-  theme_bw()
-
+vif(climate_beta)
+mean(vif(climate_beta))
