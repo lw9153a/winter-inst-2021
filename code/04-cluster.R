78192ac1f2316473d7bde5b9513837659afaabc6
London <lw9153a@student.american.edu>
2021-01-14 21:27:25 -0500

    Filtered out variables we need for clustering

--- code/04-cluster.R
+++ code/04-cluster.R
@@ -4,4 +4,12 @@ library(ClusterR)
 
 climate <- read_csv(here("data", "clean_climate.csv"))
 
+climate %>%
+  select(state, cp_2019, gdp, co2_emissions, perc_renew, perc_ff, pop, perc_white,
+         perc_black, perc_latino, perc_asian, human, margin_2016)%>%
+  mutate(gdp_pc = (gdp*1000000)/pop)%>%
+  mutate(co2_pc = (co2_emissions*1000000)/pop)%>%
+  select(-c(co2_emissions, gdp, pop))->
+  climate_num
+
 
